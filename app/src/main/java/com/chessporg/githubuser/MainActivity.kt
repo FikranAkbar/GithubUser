8277ad48e9605155b83f473fe611d7a592fcee20
Fikran Akbar <fikranakbar756@gmail.com>
2021-08-10 15:57:40 +0700

    adding header main activity

--- app/src/main/java/com/chessporg/githubuser/MainActivity.kt
+++ app/src/main/java/com/chessporg/githubuser/MainActivity.kt
@@ -1,11 +1,63 @@
 package com.chessporg.githubuser
 
+import android.content.res.TypedArray
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.chessporg.githubuser.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+
+    private lateinit var binding: ActivityMainBinding
+
+    //region DataUser
+    private lateinit var dataName: Array<String>
+    private lateinit var dataUsername: Array<String>
+    private lateinit var dataLocation: Array<String>
+    private lateinit var dataRepository: Array<String>
+    private lateinit var dataCompany: Array<String>
+    private lateinit var dataFollowers: Array<String>
+    private lateinit var dataFollowing: Array<String>
+    private lateinit var dataAvatar: TypedArray
+    private lateinit var userList: ArrayList<User>
+    //endregion
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        getData()
+        Log.d(TAG, "onCreate: Get Data Done")
+    }
+
+    private fun getData() {
+        dataName = resources.getStringArray(R.array.name)
+        dataUsername = resources.getStringArray(R.array.username)
+        dataLocation = resources.getStringArray(R.array.location)
+        dataRepository = resources.getStringArray(R.array.repository)
+        dataCompany = resources.getStringArray(R.array.company)
+        dataFollowers = resources.getStringArray(R.array.followers)
+        dataFollowing = resources.getStringArray(R.array.following)
+        dataAvatar = resources.obtainTypedArray(R.array.avatar)
+
+        userList = arrayListOf()
+        for(i in dataName.indices) {
+            val user = User(
+                name = dataName[i],
+                username = dataUsername[i],
+                location = dataLocation[i],
+                repository = dataRepository[i],
+                company = dataCompany[i],
+                followers = dataFollowers[i],
+                following = dataFollowing[i],
+                avatar = dataAvatar.getResourceId(i, -1),
+            )
+            userList.add(user)
+        }
     }
 }
