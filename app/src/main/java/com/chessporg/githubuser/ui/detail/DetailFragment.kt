2c5d67ec7a51969bfc6280ffe02a4dfd9d5cb972
Fikran Akbar <fikranakbar756@gmail.com>
2021-10-30 23:47:12 +0700

    implementing detail fragment

--- app/src/main/java/com/chessporg/githubuser/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/chessporg/githubuser/ui/detail/DetailFragment.kt
@@ -1,32 +1,34 @@
 package com.chessporg.githubuser.ui.detail
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.chessporg.githubuser.R
 import com.chessporg.githubuser.data.model.User
 import com.chessporg.githubuser.databinding.FragmentDetailBinding
+import kotlinx.coroutines.flow.collect
 
 class DetailFragment : Fragment(R.layout.fragment_detail) {
     private val viewModel: DetailViewModel by viewModels()
     private lateinit var binding: FragmentDetailBinding
-    private lateinit var userData: User
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentDetailBinding.bind(view)
 
-        setDetailScreen()
-    }
 
-    private fun setDetailScreen() {
         binding.apply {
             ivBackButton.setOnClickListener {
-
+                viewModel.onBackClick()
             }
+
+            val userData = viewModel.user!!
 
             tvUserName.text = userData.username
             tvRealName.text = userData.name
@@ -42,7 +44,24 @@ class DetailFragment : Fragment(R.layout.fragment_detail) {
             tvRepository.text = userData.repository
 
             btnShare.setOnClickListener {
+                viewModel.onShareClick(userData)
+            }
+        }
 
+        viewLifecycleOwner.lifecycleScope.launchWhenStarted {
+            viewModel.detailUserEvent.collect { event ->
+                when(event) {
+                    DetailViewModel.DetailUserEvent.NavigateBackToHome -> {
+                        findNavController().popBackStack()
+                    }
+                    is DetailViewModel.DetailUserEvent.ShareGithubUserData -> {
+                        val shareIntent = Intent()
+                        shareIntent.action = Intent.ACTION_SEND
+                        shareIntent.type = "text/plain"
+                        shareIntent.putExtra(Intent.EXTRA_TEXT, "Github Username: ${event.userData.username}")
+                        startActivity(Intent.createChooser(shareIntent, "Share Github Username to"))
+                    }
+                }
             }
         }
     }
--- app/src/main/java/com/chessporg/githubuser/ui/detail/DetailViewModel.kt
+++ app/src/main/java/com/chessporg/githubuser/ui/detail/DetailViewModel.kt
@@ -2,11 +2,31 @@ package com.chessporg.githubuser.ui.detail
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.chessporg.githubuser.data.model.User
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
 
 class DetailViewModel(
     private val state: SavedStateHandle
 ) : ViewModel() {
+
+    private val detailUserEventChannel = Channel<DetailUserEvent>()
+    val detailUserEvent = detailUserEventChannel.receiveAsFlow()
+
     val user = state.get<User>("user")
 
+    fun onBackClick() = viewModelScope.launch {
+        detailUserEventChannel.send(DetailUserEvent.NavigateBackToHome)
+    }
+
+    fun onShareClick(userData: User) = viewModelScope.launch {
+        detailUserEventChannel.send(DetailUserEvent.ShareGithubUserData(userData))
+    }
+
+    sealed class DetailUserEvent {
+        object NavigateBackToHome : DetailUserEvent()
+        data class ShareGithubUserData(val userData: User) : DetailUserEvent()
+    }
 }
