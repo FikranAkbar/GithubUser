7999aac2fc35ffcd9ab4a0de73dc4c1af66a30c7
Fikran Akbar <fikranakbar756@gmail.com>
2021-11-02 12:02:27 +0700

    add viewpager, tablayout, and fixing detail user fragment layout

--- app/src/main/java/com/chessporg/githubuser/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/chessporg/githubuser/ui/detail/DetailFragment.kt
@@ -3,18 +3,32 @@ package com.chessporg.githubuser.ui.detail
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import androidx.annotation.StringRes
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import androidx.viewpager2.widget.ViewPager2
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.chessporg.githubuser.R
 import com.chessporg.githubuser.data.model.User
 import com.chessporg.githubuser.databinding.FragmentDetailBinding
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.coroutines.flow.collect
 
 class DetailFragment : Fragment(R.layout.fragment_detail) {
+
+    companion object {
+        @StringRes
+        private val TAB_TITLES = intArrayOf(
+            R.string.tab_text_1,
+            R.string.tab_text_2
+        )
+    }
+
     private val viewModel: DetailViewModel by viewModels()
     private lateinit var binding: FragmentDetailBinding
 
@@ -22,28 +36,19 @@ class DetailFragment : Fragment(R.layout.fragment_detail) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentDetailBinding.bind(view)
 
+        viewModel.getUserDetail(viewModel.user!!)
+
+        val sectionsPagerAdapter = SectionsPagerAdapter(requireActivity() as AppCompatActivity)
+        val viewPager: ViewPager2 = view.findViewById(R.id.view_pager)
+        viewPager.adapter = sectionsPagerAdapter
+        val tabs: TabLayout = view.findViewById(R.id.tabs)
+        TabLayoutMediator(tabs, viewPager) { tab, position ->
+            tab.text = resources.getString(TAB_TITLES[position])
+        }.attach()
+
         binding.apply {
             ivBackButton.setOnClickListener {
                 viewModel.onBackClick()
-            }
-
-            val userData = viewModel.user!!
-
-            tvUserName.text = userData.username
-            tvRealName.text = userData.name
-
-            Glide.with(requireActivity())
-                .load(userData.avatar)
-                .apply(RequestOptions().override(200, 200))
-                .into(civProfilPicture)
-
-            tvFollowerFollowing.text = getString(R.string.follower_1_s_following_2_s, userData.followers, userData.following)
-            tvCompany.text = userData.company
-            tvAddress.text = userData.location
-            tvRepository.text = userData.repository
-
-            btnShare.setOnClickListener {
-                viewModel.onShareClick(userData)
             }
         }
 
@@ -60,6 +65,9 @@ class DetailFragment : Fragment(R.layout.fragment_detail) {
                         shareIntent.putExtra(Intent.EXTRA_TEXT, "Github Username: ${event.userData.username}")
                         startActivity(Intent.createChooser(shareIntent, "Share Github Username to"))
                     }
+                    is DetailViewModel.DetailUserEvent.Error -> TODO()
+                    is DetailViewModel.DetailUserEvent.Loading -> TODO()
+                    is DetailViewModel.DetailUserEvent.Success -> TODO()
                 }
             }
         }
---
+++ app/src/main/java/com/chessporg/githubuser/ui/detail/follower/FollowerFragment.kt
@@ -0,0 +1,12 @@
+package com.chessporg.githubuser.ui.detail.follower
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.chessporg.githubuser.R
+
+class FollowerFragment : Fragment(R.layout.fragment_follow) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
